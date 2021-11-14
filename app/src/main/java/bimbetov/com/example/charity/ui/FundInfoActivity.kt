f9827a58f690fd7e581d145298e01e5fa7521177
Beibarys <beimbik@gmail.com>
2021-11-14 08:29:57 +0600

    Added layout for FundInfoActivity

--- app/src/main/java/bimbetov/com/example/charity/ui/FundInfoActivity.kt
+++ app/src/main/java/bimbetov/com/example/charity/ui/FundInfoActivity.kt
@@ -2,12 +2,16 @@ package bimbetov.com.example.charity.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import bimbetov.com.example.charity.databinding.FundInfoBinding
 
 class FundInfoActivity : AppCompatActivity() {
 
+    lateinit var binding: FundInfoBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = FundInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
     }
 }
